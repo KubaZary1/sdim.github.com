eda50823242bf9d7e7b48d1fc9bedabdab81049e
KubaZary <kubamusielski@wp.pl>
2023-03-30 15:42:50 +0200

    e

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css";
 export default function Getto() {
   return (
     <footer>
-      Strona zrobiona przez Kube Musielskiego i Olafa Harasima na konkurs SDIM
+      Strona zrobiona przez Kubę Musielskiego i Olafa Harasim na konkurs SDIM
       2023
     </footer>
   );
